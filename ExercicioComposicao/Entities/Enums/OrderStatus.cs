cee06870f754a8db2a34331ccc7637c9be1b3e84
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-11 18:14:42 -0300

    Criada a pasta Enum, a enumeração OrderStatus e as classes OrderItem e Product

---
+++ ExercicioComposicao/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ExercicioComposicao.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
---
+++ ExercicioComposicao/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ExercicioComposicao.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+    }
+}
