ce3be83021db56bb17cdd6cf4144f8c96af9b365
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-13 16:11:07 -0300

    Implementada a classe Order

---
+++ ExercicioComposicao/Entities/Order.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using ExercicioComposicao.Entities.Enums;
+
+namespace ExercicioComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime date, OrderStatus status, Client client)
+        {
+            Date = date;
+            Status = status;
+            this.Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void Removeitem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem item in Items)
+            {
+                
+                sum += item.SubTotal();
+            }
+
+            return sum;
+        }
+    }
+}
