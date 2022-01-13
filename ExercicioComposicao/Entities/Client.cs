75b91ea5ac981325c065f3d3ce51fd94ecf8843d
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-13 14:49:56 -0300

    Adicionando a classe Client

---
+++ ExercicioComposicao/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioComposicao.Entities
+{
+    class Client
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
